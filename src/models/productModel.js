2fd2303fb9bb18666a8f2a0284d5f79037186594
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-09-02 09:10:07 +0600

    updated-product-name

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -3,7 +3,7 @@ const { ObjectId } = mongoose.Schema; //same as: mongoose.Schema.Types.ObjectId
 
 const productSchema = new mongoose.Schema(
 	{
-		nam: { type: String, required: ture },
+		name: { type: String, required: ture },
 		description: { type: String },
 		price: { type: Number, required: ture, min: 0.01 }, // Assuming a minimum price of 0.01 (positive value)
 		stock: { type: Number, required: ture, min: 0 }, // Ensures stock is a non-negative integer.
